dbe0bc02e94c8a2e2cfcdd49ca590bda046312aa
IcedAcorn <92429579+IcedAcorn@users.noreply.github.com>
2021-10-21 19:14:22 +0900

    네이버 로그인 기능 추가

--- src/main/java/com/jojoIdu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoIdu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,6 +27,9 @@ public class OAuthAttributes {
 
     //OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값을 하나하나 변환을 위한 메서드
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -36,6 +39,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attribute){
+        Map<String, Object> response = (Map<String, Object>) attribute.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
